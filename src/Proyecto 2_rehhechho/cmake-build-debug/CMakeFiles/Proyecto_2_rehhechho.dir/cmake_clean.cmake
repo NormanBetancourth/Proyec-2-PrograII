04ad3d219b7d6b1653ebd660c98a503d70f601d2
Servellon <servellonrebeca99@gmail.com>
2021-06-02 21:51:32 -0600

    Agregué las clases para leer y escribir en json. Por ahora solamente se puede escribir, sin embargo, hay unos detalles por ajustar

--- src/Proyecto 2_rehhechho/cmake-build-debug/CMakeFiles/Proyecto_2_rehhechho.dir/cmake_clean.cmake
+++ src/Proyecto 2_rehhechho/cmake-build-debug/CMakeFiles/Proyecto_2_rehhechho.dir/cmake_clean.cmake
@@ -2,6 +2,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/Proyecto_2_rehhechho.dir/Analyst.cpp.obj"
   "CMakeFiles/Proyecto_2_rehhechho.dir/Disease.cpp.obj"
   "CMakeFiles/Proyecto_2_rehhechho.dir/DiseaseCSVTransformer.cpp.obj"
+  "CMakeFiles/Proyecto_2_rehhechho.dir/JSONTransformer.cpp.obj"
   "CMakeFiles/Proyecto_2_rehhechho.dir/MediatorImplemented.cpp.obj"
   "CMakeFiles/Proyecto_2_rehhechho.dir/Patient.cpp.obj"
   "CMakeFiles/Proyecto_2_rehhechho.dir/PatientCSVTransformer.cpp.obj"
@@ -10,8 +11,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/Proyecto_2_rehhechho.dir/main.cpp.obj"
   "Proyecto_2_rehhechho.exe"
   "Proyecto_2_rehhechho.exe.manifest"
+  "Proyecto_2_rehhechho.lib"
   "Proyecto_2_rehhechho.pdb"
-  "libProyecto_2_rehhechho.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
--- src/Proyecto 2_rehhechho/main.cpp
+++ src/Proyecto 2_rehhechho/main.cpp
@@ -1,11 +1,27 @@
 //
 // Created by norma on 23/5/2021.
 //
+#include"Patient.h"
+#include"JSONWriter.h"
 #include "MediatorImplemented.h"
+
 int main() {
+    IWriter<Patient>* writer = new JSONWriter<Patient>("pruebaJSON.json");
+    auto* objeto1 = new Patient{ "Rebe", "123", "ABC", new SimpleArrayTemplate<string>() };
+    auto* objeto2 = new Patient{ "Norman", "456", "DEF", new SimpleArrayTemplate<string>() };
+
+    writer->write(objeto1);
+    writer->write(objeto2);
+
+    delete objeto1;
+    delete objeto2;
+    delete writer;
+
+
+    /*
     MediatorImplemented* M = new MediatorImplemented();
     M->menu();
-    return 0;
+    return 0;*/
 }
 
 
