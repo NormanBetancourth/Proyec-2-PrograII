6287c789b05140960c7ec9f70fde60f22265316e
NormanBetancourth <norman.betancourtt.mairena@est.una.ac.cr>
2021-06-04 18:35:38 -0600

    explicacion de Iterador

--- src/Proyecto 2_rehhechho/main.cpp
+++ src/Proyecto 2_rehhechho/main.cpp
@@ -12,9 +12,13 @@ int main() {
     Control* M = new Control();
     M->menu();
     /**
-     * Usamos el iterador para respetar la generalidad del template, y realizamos
+     * Usamos el iterador para respetar la generalidad del Arraytemplate, y realizamos
      * los metodos especificos necesarios para cada clase fuera, en analyst, asi
-     * evitamos problemas de ejecucion y mantenemos la generalidad del ArrayTemplate
+     * evitamos problemas de ejecucion y mantenemos la generalidad del ArrayTemplate.
+     * Aunque en este proyecto solo usamos una estructura de Arraytemplate, es importante
+     * resaltar que el programa queda abierto a alguna mejora, con alguna otra estructura
+     * generica, a la cual podemos acceder a todos los elementos mediante una corta implementacon
+     * de su respectivo Iterador.
      * **/
 
     return 0;
